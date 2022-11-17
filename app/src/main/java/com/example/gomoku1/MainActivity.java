a1f3885ff6d62171e238a635788347459767faa6
Ngô Phi Hùng <hungngophi32@gmail.com>
2022-11-17 13:45:31 +0700

    Winner  Process
    Some function in this commit:
    - Check winner
    - Dialog show winner

--- app/src/main/java/com/example/gomoku1/MainActivity.java
+++ app/src/main/java/com/example/gomoku1/MainActivity.java
@@ -71,11 +71,102 @@ public class MainActivity extends AppCompatActivity {
     }
     private void player1Turn(){
         tvTurn.setText("Turn of: Player 1");
-        if(firstMove){
-            firstMove=false;
-            makeMove();
-        }else{
+    }
+    //
+    // Dialog thông báo chiến thắng
+    public void openDialog(){
+        DialogWinner dialogWinner=new DialogWinner();
+        dialogWinner.show(getSupportFragmentManager(), "HIHI");
+    }
+    //
+    private boolean checkWinner(){
+        if (winnerPlay!=0) return true;
+        VectorEnd(xMove,0,0,1,xMove,yMove);
+        if (winnerPlay!=0) return true;
 
+        VectorEnd(0,yMove,1,0,xMove,yMove);
+        if (winnerPlay!=0) return true;
+
+        if(xMove+yMove>=maxN-1){
+            VectorEnd(maxN-1,xMove+yMove-maxN+1,-1,1,xMove,yMove);
+        }
+        else if(xMove<=yMove){
+            VectorEnd(xMove-yMove+maxN-1,maxN-1,-1,-1,xMove,yMove);
+        }else{
+            VectorEnd(maxN-1,maxN-1-(xMove-yMove),-1,-1,xMove,yMove);
+        }
+        if(winnerPlay!=0) return  true;
+        return false;
+    }
+    private void VectorEnd(int xx,int yy,int vx,int vy,int rx,int ry){
+        if (winnerPlay!=0){
+            return;
+        }
+        final int range=4;
+        int i,j;
+        int xbelow=rx-range*vx;
+        int ybelow=ry-range*vy;
+        int xabove=rx+range*vx;
+        int yabove=ry+range*vy;
+        String st="";
+        i=xx;
+        j=yy;
+        while (!inside(i,xbelow,xabove)|| !inside(j,ybelow,yabove)){
+            i+=vx;
+            j+=vy;
+        }
+        while (true){
+            st=st+String.valueOf(valueCell[i][j]);
+            if(st.length()==5){
+                EvalEnd(st);
+                if(winnerPlay!=0){
+                    break;
+                }
+                st=st.substring(1,5);
+            }
+            i+=vx;
+            j+=vy;
+            if(!inBoard(i,j)|| !inside(i,xbelow,xabove) || !inside(j,ybelow,yabove)){
+                break;
+            }
+        }
+    }
+    private boolean inBoard(int i,int j){
+        if(i<0||i>=maxN|| j<0 || j>=maxN){
+            return false;
+        }
+        return true;
+    }
+    private void EvalEnd(String st){
+        Toast.makeText(context,st,Toast.LENGTH_SHORT).show();
+
+        switch (st){
+            case "11111":
+                winnerPlay=1;
+                break;
+            case "22222":
+                winnerPlay=2;
+                break;
+            default:
+                break;
+        }
+    }
+    private boolean inside(int i,int xd, int xt){
+        return (i-xd)*(i-xt)<=0;
+    }
+    private boolean noEmptyCell(){
+        for (int i=0;i<maxN;i++){
+            for (int j=0;j<maxN;j++){
+                if(valueCell[i][j]==0) return false;
+            }
+        }
+        return true;
+    }
+    private void setValueCell(){
+        for (int i=0;i<maxN;i++){
+            for (int j=0;j<maxN;j++){
+                valueCell[i][j]=0;
+            }
         }
     }
     private void player2Turn(){
@@ -83,12 +174,34 @@ public class MainActivity extends AppCompatActivity {
     }
     private void makeMove(){
         cell[xMove][yMove].setImageDrawable(drawCell[turnPlay]);
+        valueCell[xMove][yMove]=turnPlay;
+        if(noEmptyCell()){
+            Toast.makeText(context,"DRAW!!!",Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            if(checkWinner()){
+                if (winnerPlay==1) {
+                    Toast.makeText(context, "Winner is Player 1"  , Toast.LENGTH_SHORT).show();
+
+                }else if (winnerPlay==2){
+                    Toast.makeText(context, "Winner is Player 2" , Toast.LENGTH_SHORT).show();
+
+                }
+                openDialog();
+
+                return;
+            }
+        }
 
         if(turnPlay==1){
             turnPlay=3-turnPlay;
+            Toast.makeText(context,"HIHI",Toast.LENGTH_SHORT).show();
+
             player2Turn();
         }else{
             turnPlay=3-turnPlay;
+            Toast.makeText(context,"Haha",Toast.LENGTH_SHORT).show();
+
             player1Turn();
         }
     }
@@ -113,10 +226,15 @@ public class MainActivity extends AppCompatActivity {
                 final int y=j;
                 cell[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
+
                     public void onClick(View view) {
-                        xMove=x;
-                        yMove=y;
-                        makeMove();
+
+                        if(valueCell[x][y]==0) {
+                            xMove = x;
+                            yMove = y;
+
+                            makeMove();
+                        }
                     }
                 });
                 lnRow.addView(cell[i][j],lpcell);
